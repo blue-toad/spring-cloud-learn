eeaeed39403b741e51f927130a391391db688042
段东桀 <61642973+blue-toad@users.noreply.github.com>
2020-11-07 17:50:25 +0800

    Hystrix服务降级

--- consumer-hystrix/src/main/java/com/kinopio/springcloud/HystrixMain.java
+++ consumer-hystrix/src/main/java/com/kinopio/springcloud/HystrixMain.java
@@ -3,13 +3,15 @@ package com.kinopio.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients //启动openfeign
 @EnableEurekaClient
-public class FeignMain {
-    public static void main(String args[]){
-        SpringApplication.run(FeignMain.class, args);
+@EnableFeignClients
+@EnableHystrix
+public class HystrixMain {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixMain.class, args);
     }
 }
